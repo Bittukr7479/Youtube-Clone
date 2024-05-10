3f7e4b873e0ac9dc6b3a715426a0841bceb35aef
Bittu <123286618+Bittukr7479@users.noreply.github.com>
2024-05-10 20:35:27 +0530

    commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { categories } from '../utils/constants';
+import { Stack } from '@mui/material'
+const Sidebar = () => {
+  return (
+    <Stack direction="row" sx={{ overflowY: "auto",  height: { sx: "auto, md: '95%" },  flexDirection: { md: 'column' }}} >
+      {categories.map((category) => (
+        <button className='category-btn'>
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  )
+}
+
+export default Sidebar;
